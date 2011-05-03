a1dd0d7d9c329ff2c700ef446f973851939c8061
Archerfly <qqq26299010@hotmail.com>
2011-05-04 00:55:29 +0800

    吳明珠中醫診所API source code

--- src/helloworld.py
+++ src/helloworld.py
@@ -14,7 +14,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 from django.utils import simplejson as json
-
+ 
 
 class result(db.Model):
     url = db.TextProperty()
